2761ba8621def5f3025422105649f33e07a0b855
nastyarozhina <Sapphire_owl@outlook.com>
2026-03-02 04:06:29 +0300

    Create a web-page with a pet-catalog (edit styles, layout.tpl, working with json in routes.py)

--- fluffy_corner/routes.py
+++ fluffy_corner/routes.py
@@ -3,12 +3,15 @@ Routes and views for the bottle application.
 """
 
 from bottle import route, view, template, redirect
+import json
 
 @route('/')
 @route('/pets')
 @view('find_pet')
 def home():
-    """Renders the home page."""
+    with open(r"static\pets.json", "r", encoding="utf-8-sig") as pet_data:
+            data = json.load(pet_data)
     return dict(
-        title = 'Питомцы'
+        title = 'Питомцы',
+        pets=data
     )
