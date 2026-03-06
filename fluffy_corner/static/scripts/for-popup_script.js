a6622b371d7f0097b9d0f8d4d7bcef38bd98a6fa
nastyarozhina <Sapphire_owl@outlook.com>
2026-03-06 04:27:20 +0300

    Create popup where information about how to get a pet

---
+++ fluffy_corner/static/scripts/for-popup_script.js
@@ -0,0 +1,9 @@
+function openPopup() {
+    document.getElementById("popup").style.display = "block";
+    document.body.style.overflow = "hidden";
+}
+
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+    document.body.style.overflow = "auto";
+}
